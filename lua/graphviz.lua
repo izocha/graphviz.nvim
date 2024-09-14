06c7d79a041a32ba4de2e30a26442ed7a7e175ac
izocha <isaias.carrillo@outlook.es>
2024-09-14 17:11:23 -0500

    fix(command): Graphs commands now works on every dot file
    Fixed an issue where commands were only generated in the first .dot file opened. Commands now work using autocmd instead of checking vim.bo.filetype, and should work in any .dot file.

--- lua/graphviz.lua
+++ lua/graphviz.lua
@@ -17,25 +17,28 @@ end
 M.user_commands = function()
 	local buf_user_command = vim.api.nvim_buf_create_user_command
 	-- Create commands ONLY for dot files
-	if vim.bo.filetype == "dot" then
-		buf_user_command(0, "GraphExport", function(opts)
-			local args = opts.fargs[1] or M.config.format
-			commands.graph_export(args)
-		end, {
-			desc = "Export graph",
-			--NOTE:-- Why "*"?, because this allow me to use default values
-			nargs = "*",
-			complete = function()
-				return utils.args_complete()
-			end,
-		})
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = "dot",
+		callback = function()
+			buf_user_command(0, "GraphExport", function(opts)
+				local args = opts.fargs[1] or M.config.format
+				commands.graph_export(args)
+			end, {
+				desc = "Export graph",
+				--NOTE:-- Why "*"?, because this allow me to use default values
+				nargs = "*",
+				complete = function()
+					return utils.args_complete()
+				end,
+			})
 
-		buf_user_command(0, "GraphPreview", function()
-			commands.graph_preview(M.config.preview)
-		end, {
-			desc = "Graphviz preview with autocommand",
-		})
-	end
+			buf_user_command(0, "GraphPreview", function()
+				commands.graph_preview(M.config.preview)
+			end, {
+				desc = "Graphviz preview with autocommand",
+			})
+		end,
+	})
 end
 
 return M
